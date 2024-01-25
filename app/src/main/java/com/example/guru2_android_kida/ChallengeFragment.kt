41a3614287e4f5f627c6b4276b6f9a25e9270d28
Moon-ye-rin <myr1106@naver.com>
2024-01-25 18:26:36 +0900

    create 메뉴바, 해당하는 fragment, 페이지 연결

---
+++ app/src/main/java/com/example/guru2_android_kida/ChallengeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.guru2_android_kida
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// 챌린지 액티비티 입니다.
+class ChallengeFragment : Fragment(R.layout.fragment_challenge) {
+
+}
---
+++ app/src/main/java/com/example/guru2_android_kida/MyPageFragment.kt
@@ -0,0 +1,12 @@
+package com.example.guru2_android_kida
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// 마이페이지 액티비티 입니다.
+class MyPageFragment : Fragment(R.layout.fragment_my_page) {
+
+}
