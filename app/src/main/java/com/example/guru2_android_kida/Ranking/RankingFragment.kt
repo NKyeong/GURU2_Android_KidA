6051189b8710bf80c213d0b510b08b9a42399abd
lucy539090 <105434003+lucy539090@users.noreply.github.com>
2024-01-30 15:45:37 +0900

    Update RankingFragment.kt

--- app/src/main/java/com/example/guru2_android_kida/Ranking/RankingFragment.kt
+++ app/src/main/java/com/example/guru2_android_kida/Ranking/RankingFragment.kt
@@ -1,47 +1,52 @@
 package com.example.guru2_android_kida.Ranking
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.guru2_android_kida.databinding.FragmentRankingBinding
 import java.text.SimpleDateFormat
 import java.util.*
-import com.example.guru2_android_kida.databinding.FragmentRankingBinding
-import java.util.Calendar
-import java.util.Locale
 
 class RankingFragment : Fragment() {
 
-    // FragmentRankingBinding을 사용하여 뷰 바인딩 초기화
-    private val binding: FragmentRankingBinding by lazy {
-        FragmentRankingBinding.inflate(layoutInflater)
-    }
+    private lateinit var binding: FragmentRankingBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentRankingBinding.inflate(inflater, container, false)
+        binding.recyclerViewRanking.layoutManager = LinearLayoutManager(context) // 여기에 추가
         updateCurrentMonth()
         setupRankingList()
         return binding.root
     }
 
-    // 전 월을 업데이트하는 함수
     private fun updateCurrentMonth() {
         val calendar = Calendar.getInstance()
         calendar.add(Calendar.MONTH, -1)
-        val lastMonth = SimpleDateFormat("MMMM", Locale.getDefault()).format(calendar.time)
-        binding.tvCurrentMonth.text = "#${lastMonth}의 랭킹"
+        val lastMonth = SimpleDateFormat("MM", Locale.getDefault()).format(calendar.time)
+        binding.tvCurrentMonth.text = "#${lastMonth}월의 랭킹"
     }
 
-    // 랭킹 리스트를 설정하는 함수
     private fun setupRankingList() {
-        // 랜덤 사용자 데이터 생성
-        val rankingData = List(50) { userRanking("User ${it + 1}", Random().nextInt(100)) }
+        val random = Random()
+
+        // 랜덤한 이름과 도장 개수를 가진 유저 데이터 생성
+        val rankingData = List(50) {
+            UserRanking("User ${random.nextInt(100)}", (1..100).random())
+        }
+
         // 도장 개수에 따라 내림차순으로 정렬
         val sortedRankingData = rankingData.sortedByDescending { it.stampsCount }
+
+        // 어댑터에 정렬된 데이터 전달
         val adapter = UserRankingAdapter(sortedRankingData)
         binding.recyclerViewRanking.adapter = adapter
+
+        // 로그로 데이터 크기 및 어댑터 설정 확인
+        Log.d("RankingFragment", "Ranking data size: ${rankingData.size}")
+        Log.d("RankingFragment", "Adapter is set")
     }
 }
