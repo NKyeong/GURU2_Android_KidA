9efe7f28842def5e52a5f4ab3da90775e3883fc8
NKyeong <nakyeong1121@gmail.com>
2024-01-30 17:00:49 +0900

    my_page

--- app/src/main/java/com/example/guru2_android_kida/MyPage/MyPageEditFragment.kt
+++ app/src/main/java/com/example/guru2_android_kida/MyPage/MyPageEditFragment.kt
@@ -41,16 +41,5 @@ class MyPageEditFragment : AppCompatActivity() {
     private fun deleteSelectedChallenge(challengeId: Long) {
         // 이 부분에 선택한 챌린지를 삭제하는 코드를 추가
         //dbHelper.deleteChallenge(challengeId)
-
-        // DBHelper에 추가
-        // DBHelper 클래스에서 챌린지를 삭제하는 함수
-        /*fun deleteChallenge(challengeId: Long): Int {
-            val db = this.writableDatabase
-            val whereClause = "$COLUMN_ID = ?" // COLUMN_ID는 챌린지의 ID 컬럼명으로 수정해야 함
-            val whereArgs = arrayOf(challengeId.toString())
-
-            // 삭제된 행의 수를 반환
-            return db.delete(TABLE_CHALLENGES, whereClause, whereArgs)
-        }*/
     }
 }
--- app/src/main/java/com/example/guru2_android_kida/MyPage/MyPageFragment.kt
+++ app/src/main/java/com/example/guru2_android_kida/MyPage/MyPageFragment.kt
@@ -1,9 +1,16 @@
 package com.example.guru2_android_kida.MyPage
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import android.widget.Button
 import android.widget.TextView
-import com.example.guru2_android_kida.Login.DBHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.guru2_android_kida.Challenge.ChallengeDBHelper
 import com.example.guru2_android_kida.R
 import com.example.guru2_android_kida.databinding.FragmentMyPageBinding
 
@@ -14,11 +21,11 @@ class MyPageFragment : Fragment(R.layout.fragment_my_page) {
         FragmentMyPageBinding.inflate(layoutInflater)
     }
 
-    private lateinit var dbHelper: DBHelper
+    private lateinit var ChallengeDBHelper: ChallengeDBHelper
     private lateinit var userName: TextView
     private lateinit var btnEdit: Button
 
-    /*override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
@@ -30,34 +37,32 @@ class MyPageFragment : Fragment(R.layout.fragment_my_page) {
         btnEdit = binding.btnEdit
 
         // MyDB에서 특정 username 불러오기
-        dbHelper = DBHelper(requireContext())
+        //dbHelper 초기화
+        //dbHelper = DBHelper(requireContext())
 
         // 특정 username을 지정하여 사용자 정보를 가져옴
-        val username = 1L // 예시로 username를 1로 설정
-        val user = dbHelper.getUser(username)
+        /*val username = 1L // 예시로 username를 1로 설정
+        val user = ChallengeDBHelper.getUser(username)
 
         if (user != null) {
             // 사용자 정보를 사용하여 원하는 작업 수행
             // 예시로 로그 출력
-            userName.text = user.username}
+            userName.text = user.username}*/
 
 
         // 레벨 불러오기
 
 
 
-        // challenge 데이터베이스에서 진행중인 챌린지 불러오기
+        // ChallengeDBHelper에서 진행중인 챌린지 불러오기
         // 챌린지 정보를 담을 모델 클래스 만들기
-        // 연결된 dbHelper는 login에 대한 DB라서 challenge에 관한 databaseHelper를 만들어야함
-        // 추가로 어댑터도 만들어야함
-        val challengeView: RecyclerView = binding.challengeView
+        /*val challengeView: RecyclerView = binding.challengeView
         challengeView.layoutManager = LinearLayoutManager(requireContext())
 
-        val challengeList = dbHelper.getChallenges()
+        val challengeList = ChallengeDBHelper.getJoinedChallengesForUser()
 
         val adapter = ChallengeAdapter(challengeList)
-        challengeView.adapter = adapter
-
+        challengeView.adapter = adapter*/
 
         // 수정하는 페이지(activity_my_page_edit)로 이동
         btnEdit.setOnClickListener {
@@ -66,13 +71,5 @@ class MyPageFragment : Fragment(R.layout.fragment_my_page) {
         }
 
         return view
-    }*/
-
-
-    // 다른 페이지들과 연결
-    // fragment_my_page에서 네비게이션 바 생성하고 연결하기
-    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.my_page, menu)
-        return true
-    }*/
+    }
 }
