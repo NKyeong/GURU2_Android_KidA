73147c5d39c14e81349ff67d340d8f5e0cb1a857
NKyeong <nakyeong1121@gmail.com>
2024-02-02 02:27:32 +0900

    my_page

--- app/src/main/java/com/example/guru2_android_kida/MyPage/MyPageEditActivity.kt
+++ app/src/main/java/com/example/guru2_android_kida/MyPage/MyPageEditActivity.kt
@@ -9,27 +9,26 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.guru2_android_kida.R
+import com.example.guru2_android_kida.databinding.ActivityMyPageEditBinding
 import com.example.guru2_android_kida.databinding.FragmentMyPageBinding
 
-class MyPageEditActivity: Fragment(R.layout.activity_my_page_edit) {
+class MyPageEditActivity: AppCompatActivity() {
 
-    private val binding: FragmentMyPageBinding by lazy {
-        FragmentMyPageBinding.inflate(layoutInflater)
-    }
+    private lateinit var binding: ActivityMyPageEditBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyPageEditBinding.inflate(layoutInflater)
 
         val view = binding.root
 
-        /*binding.btnRemove.setOnClickListener {
-            val intent = Intent(requireContext(), MyPageFragment::class.java)
+        binding.btnRemove.setOnClickListener {
+            // MyPageFragment로 이동하는 부분
+            val intent = Intent(this, MyPageFragment::class.java)
             startActivity(intent)
-        }*/
+        }
 
-        return view
+        setContentView(view)
 
     }
 }
--- app/src/main/java/com/example/guru2_android_kida/MyPage/MyPageFragment.kt
+++ app/src/main/java/com/example/guru2_android_kida/MyPage/MyPageFragment.kt
@@ -7,26 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.guru2_android_kida.Challenge.ChallengeAdapter
 import com.example.guru2_android_kida.Challenge.ChallengeDBHelper
 import com.example.guru2_android_kida.HomeDetail.ChallengeNameAdapter
-import com.example.guru2_android_kida.Login.DBHelper
 import com.example.guru2_android_kida.R
 import com.example.guru2_android_kida.databinding.FragmentMyPageBinding
 
 // 마이페이지 액티비티 입니다.
-class MyPageFragment : Fragment(R.layout.fragment_my_page) {
+class MyPageFragment : Fragment(R.layout.fragment_my_page), ChallengeNameAdapter.OnChallengeClickListener {
 
     private val binding: FragmentMyPageBinding by lazy {
         FragmentMyPageBinding.inflate(layoutInflater)
     }
 
-    private lateinit var challengeDBHelper: ChallengeDBHelper
-    private lateinit var loginDBHelper: DBHelper
     private lateinit var ChallengeNamedbHelper: ChallengeDBHelper
     private lateinit var userName: TextView
     private lateinit var challengeView: RecyclerView
@@ -43,15 +38,16 @@ class MyPageFragment : Fragment(R.layout.fragment_my_page) {
         val view = binding.root
         userName = binding.userName
 
-        // TextView 및 Button 초기화
-        challengeView = binding.challengeView
-
         // ChallengeDBHelper 초기화
         ChallengeNamedbHelper = ChallengeDBHelper(requireContext())
 
-        // ChallengeNameAdapter 초기화
-        //challengeNameAdapter = ChallengeNameAdapter()
+        // TextView 및 Button 초기화
+        challengeView = binding.challengeView
 
+        // ChallengeNameAdapter를 적용하도록 추가
+        challengeNameAdapter = ChallengeNameAdapter(challengeNameLIst, ChallengeNamedbHelper, this)
+        challengeView.layoutManager = LinearLayoutManager(requireContext())
+        challengeView.adapter = challengeNameAdapter
 
         // username 불러오기
         setUserNameInView()
@@ -60,7 +56,6 @@ class MyPageFragment : Fragment(R.layout.fragment_my_page) {
 
         // 사용자가 참여 중인 챌린지 정보 가져오기
         showChallengeList()
-
 
         // 수정하는 페이지(activity_my_page_edit)로 이동
         binding.btnEdit.setOnClickListener {
@@ -71,7 +66,7 @@ class MyPageFragment : Fragment(R.layout.fragment_my_page) {
         return view
     }
 
-    //// MyPageFragment 내에서 호출되는 함수로 TextView에 값 설정하기
+    // MyPageFragment 내에서 호출되는 함수로 TextView에 값 설정하기
     private fun setUserNameInView() {
         val username = getCurrentUsername()
         userName.text = username
@@ -104,5 +99,9 @@ class MyPageFragment : Fragment(R.layout.fragment_my_page) {
         challengeNameLIst.addAll(newChallengeList)
         // RecyclerView 갱신
         //notifyDataSetChanged()
+    }
+
+    override fun onChallengeClick(challengeName: String) {
+        // 클릭 이벤트를 처리합니다.
     }
 }
